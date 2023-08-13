954b23b1009b0ac168c815279714cd5f39cd6020
zhixiangChen <842930917@qq.com>
2023-08-13 19:14:29 +0800

    08.13

--- demo3-1/src/App.js
+++ demo3-1/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// 1.组件名 <Profile /> 必须以大写 P 开头，以区分 HTML 标签
+function Profile() {
+  // 2.返回 JSX 标签
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <section>
+      <h1>了不起的科学家</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
---
+++ demo3-3/src/App.js
@@ -0,0 +1,31 @@
+
+// 提取一个 Card 组件，并使用 children prop 将不同的 JSX 传递给它
+function Card({ children, title }) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default function Profile() {
+  return (
+    <div>
+      <Card title="Photo">
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={100}
+          height={100}
+        />
+      </Card>
+      <Card title="About">
+        <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+      </Card>
+    </div>
+  );
+}
